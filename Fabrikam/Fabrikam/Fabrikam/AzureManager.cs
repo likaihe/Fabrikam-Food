6117089fb23dc3825d459dd35eabce6776efe0af
likaihe <1047561425@qq.com>
2016-11-21 17:18:26 +1300

    try to install 3rd library

---
+++ Fabrikam/Fabrikam/Fabrikam/AzureManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace Fabrikam
+{
+    class AzureManager
+    {
+        private static AzureManager instance;
+        private MobileServiceClient client;
+       // private IMobileServiceTable<Timeline> timelineTable;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("http://hellotheretest.azurewebsites.net");
+            //this.timelineTable = this.client.GetTable<Timeline>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+    }
+}
